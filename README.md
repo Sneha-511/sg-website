15e475af3e25356225d6fc637914413f7e983c3f
Sneha Jain <snehavinayaka511@gmail.com>
2022-02-23 14:00:00 +0100

    Added all files

---
+++ README.md
@@ -0,0 +1,9 @@
+<h2>The website for IIT (ISM) Gymkhana is hosted at <br> https://Sneha-511.github.io/ismClubs/ </h2>
+<br>    
+ </p>
+ <p> <h4>Instructions to use: <br> </h4>
+ <ol>
+  <li> To add events in the calendar, user needs to add a JSON object in events.json </li>
+  <li> To add more search options, more JSON objects need to be created in search.js </li>
+  </ol>
+  </p>
---
+++ app.js
@@ -0,0 +1,39 @@
+const searchInput = document.querySelector(".form-control");
+var searchList = document.createElement("ul");
+searchList.className = "list-group";
+document.querySelector("main").insertBefore(searchList, document.querySelector("main").firstChild);
+const searchResults = document.querySelector(".list-group");
+searchInput.addEventListener("keyup", search);
+function add(obj) {
+    const li = document.createElement('li');
+    li.className = "list-group-item";
+    searchResults.style.position = "fixed";
+    searchResults.style.zIndex = "104";
+    searchResults.style.width = "92.5%";
+    const link = document.createElement('a');
+    link.innerHTML = `Name : ${obj.field} <br> Type : ${obj.type} <br>Website : <a href="${obj.website}">Go there </a> <br>`;
+    li.appendChild(link);
+    searchResults.appendChild(li);    
+}
+
+function reset(){
+    searchResults.innerHTML = "";
+
+}
+
+searchInput.addEventListener("blur", setTimeOut(reset,1500));
+
+function search(e) {
+    searchResults.innerHTML = "";
+    const searchValue = e.target.value.toLowerCase();
+    if(searchValue !== ""){
+        data.forEach(function(item){
+            if(item.field.toLowerCase().indexOf(searchValue)!== -1){
+                add(item);                       
+            }
+            else if(item.type.toLowerCase().indexOf(searchValue)!== -1){
+                add(item);                       
+}        })
+    }
+    e.preventDefault();
+}
